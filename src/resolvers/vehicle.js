ff3372c80e814e9266edef25cfce4e44417be9aa
Kuba <jakub_mazurek96@onet.pl>
2019-08-23 14:30:47 +0200

    refactor: reduce repeated code in vehicle resolvers

--- src/resolvers/vehicle.js
+++ src/resolvers/vehicle.js
@@ -1,56 +1,59 @@
 import ConnectedVehicleStatus from '../adapters/connectedVehicleStatus';
 
-export default scope => ({
-  Query: {
-    getVehicle: async (parent, { id: vehicleID }, { authToken }) => {
-      const data = await ConnectedVehicleStatus.withScope(scope)
-        .withToken(authToken)
-        .get_vehicleinfo(vehicleID);
-      return data;
+export default scope => () => {
+  const ConnectedVehicleStatusAdapter = ConnectedVehicleStatus.withScope(scope);
+  return {
+    Query: {
+      getVehicle: async (parent, { id: vehicleID }, { authToken }) => {
+        const data = await ConnectedVehicleStatusAdapter.withToken(
+          authToken
+        ).get_vehicleinfo(vehicleID);
+        return data;
+      },
+      vehicles: async (_, __, { authToken }) => {
+        const data = await ConnectedVehicleStatusAdapter.withToken(
+          authToken
+        ).get_allvehicles();
+        return data;
+      },
     },
-    vehicles: async (_, __, { authToken }) => {
-      const data = await ConnectedVehicleStatus.withScope(scope)
-        .withToken(authToken)
-        .get_allvehicles();
-      return data;
+    Vehicle: {
+      stateofcharge: async ({ id: vehicleID }, _, { authToken }) => {
+        const { stateofcharge } = await ConnectedVehicleStatusAdapter.withToken(
+          authToken
+        ).get_stateofcharge(vehicleID);
+        return stateofcharge;
+      },
+      location: async ({ id: vehicleID }, _, { authToken }) => {
+        const location = await ConnectedVehicleStatusAdapter.withToken(
+          authToken
+        ).get_location(vehicleID);
+        return location;
+      },
+      tires: async ({ id: vehicleID }, _, { authToken }) => {
+        const tires = await ConnectedVehicleStatusAdapter.withToken(
+          authToken
+        ).get_tires(vehicleID);
+        return tires;
+      },
+      doors: async ({ id: vehicleID }, _, { authToken }) => {
+        const doors = await ConnectedVehicleStatusAdapter.withToken(
+          authToken
+        ).get_doors(vehicleID);
+        return doors;
+      },
+      odometer: async ({ id: vehicleID }, _, { authToken }) => {
+        const odometer = await ConnectedVehicleStatusAdapter.withToken(
+          authToken
+        ).get_odometer(vehicleID);
+        return odometer;
+      },
+      fuel: async ({ id: vehicleID }, _, { authToken }) => {
+        const fuel = await ConnectedVehicleStatusAdapter.withToken(
+          authToken
+        ).get_fuel(vehicleID);
+        return fuel;
+      },
     },
-  },
-  Vehicle: {
-    stateofcharge: async ({ id: vehicleID }, _, { authToken }) => {
-      const { stateofcharge } = await ConnectedVehicleStatus.withScope(scope)
-        .withToken(authToken)
-        .get_stateofcharge(vehicleID);
-      return stateofcharge;
-    },
-    location: async ({ id: vehicleID }, _, { authToken }) => {
-      const location = await ConnectedVehicleStatus.withScope(scope)
-        .withToken(authToken)
-        .get_location(vehicleID);
-      return location;
-    },
-    tires: async ({ id: vehicleID }, _, { authToken }) => {
-      const tires = await ConnectedVehicleStatus.withScope(scope)
-        .withToken(authToken)
-        .get_tires(vehicleID);
-      return tires;
-    },
-    doors: async ({ id: vehicleID }, _, { authToken }) => {
-      const doors = await ConnectedVehicleStatus.withScope(scope)
-        .withToken(authToken)
-        .get_doors(vehicleID);
-      return doors;
-    },
-    odometer: async ({ id: vehicleID }, _, { authToken }) => {
-      const odometer = await ConnectedVehicleStatus.withScope(scope)
-        .withToken(authToken)
-        .get_odometer(vehicleID);
-      return odometer;
-    },
-    fuel: async ({ id: vehicleID }, _, { authToken }) => {
-      const fuel = await ConnectedVehicleStatus.withScope(scope)
-        .withToken(authToken)
-        .get_fuel(vehicleID);
-      return fuel;
-    },
-  },
-});
+  };
+};
